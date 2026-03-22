f7c820947a38f833f7c1343e62dad23652b9eede
RSKyo <nimeimiao@gmail.com>
2026-03-23 00:30:38 +0800

    cdp

--- node/cli.js
+++ node/cli.js
@@ -1,6 +1,12 @@
 import { run } from './infra/protocol.js';
 import { parseCliCommand } from './infra/parse.js';
+import { ACTION_COMMANDS } from './cmd/action.js';
+import { DOM_COMMANDS } from './cmd/dom.js';
+import { EMU_COMMANDS } from './cmd/emu.js';
+import { INPUT_COMMANDS } from './cmd/input.js';
+import { PAGE_COMMANDS } from './cmd/page.js';
 import { TARGET_COMMANDS } from './cmd/target.js';
+import { WAIT_COMMANDS } from './cmd/wait.js';
 
 // 一级命令分组
 const GROUP_COMMANDS = {
